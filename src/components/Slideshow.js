3db0b53507af2786e0c66806cd6a799fa3b5e776
basoosh <basoosh1@gmail.com>
2024-03-25 23:37:51 -0400

    Daily slideshow and new images

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,40 +1,131 @@
 import React from 'react';
 
-const images = [
-    "https://donut-dashboard.com/01.png", 
-    "https://donut-dashboard.com/02.png", 
-    "https://donut-dashboard.com/03.png", 
-    "https://donut-dashboard.com/04.png", 
-    "https://donut-dashboard.com/05.png", 
-    "https://donut-dashboard.com/06.png", 
-    "https://donut-dashboard.com/07.png", 
-    "https://donut-dashboard.com/08.png", 
-    "https://donut-dashboard.com/09.png", 
-    "https://donut-dashboard.com/10.png", 
-    "https://donut-dashboard.com/11.png", 
-    "https://donut-dashboard.com/12.png", 
-    "https://donut-dashboard.com/13.png", 
-    "https://donut-dashboard.com/14.png", 
-    "https://donut-dashboard.com/15.png", 
-    "https://donut-dashboard.com/16.png", 
-    "https://donut-dashboard.com/17.png", 
-    "https://donut-dashboard.com/18.png", 
-    "https://donut-dashboard.com/19.png", 
-    "https://donut-dashboard.com/20.png", 
-    "https://donut-dashboard.com/21.png", 
-    "https://donut-dashboard.com/22.png", 
-    "https://donut-dashboard.com/23.png", 
-    "https://donut-dashboard.com/24.png", 
-    "https://donut-dashboard.com/25.png", 
-    "https://donut-dashboard.com/26.png", 
-    "https://donut-dashboard.com/27.png", 
-    "https://donut-dashboard.com/28.png", 
-    "https://donut-dashboard.com/29.png", 
-    "https://donut-dashboard.com/30.png", 
-    "https://donut-dashboard.com/31.png", 
-    "https://donut-dashboard.com/32.png", 
-    "https://donut-dashboard.com/33.png", 
+const prefix = "https://donut-dashboard.com/";
+const postfix = ".png";
+
+let images = [prefix + "01" + postfix];
+
+const sundayImages = [
+  prefix + "01" + postfix,
+  prefix + "08" + postfix,
+  prefix + "15" + postfix,
+  prefix + "22" + postfix,
+  prefix + "29" + postfix,
+  prefix + "36" + postfix,
+  prefix + "43" + postfix,
+  prefix + "50" + postfix,
+  prefix + "57" + postfix,
+  prefix + "64" + postfix,
+  prefix + "71" + postfix,
+  prefix + "78" + postfix,
 ];
+
+const mondayImages = [
+  prefix + "02" + postfix,
+  prefix + "09" + postfix,
+  prefix + "16" + postfix,
+  prefix + "23" + postfix,
+  prefix + "30" + postfix,
+  prefix + "37" + postfix,
+  prefix + "44" + postfix,
+  prefix + "51" + postfix,
+  prefix + "58" + postfix,
+  prefix + "65" + postfix,
+  prefix + "72" + postfix,
+];
+
+const tuesdayImages = [
+  prefix + "03" + postfix,
+  prefix + "10" + postfix,
+  prefix + "17" + postfix,
+  prefix + "24" + postfix,
+  prefix + "31" + postfix,
+  prefix + "38" + postfix,
+  prefix + "45" + postfix,
+  prefix + "52" + postfix,
+  prefix + "59" + postfix,
+  prefix + "66" + postfix,
+  prefix + "73" + postfix,
+];
+
+const wednesdayImages = [
+  prefix + "04" + postfix,
+  prefix + "11" + postfix,
+  prefix + "18" + postfix,
+  prefix + "25" + postfix,
+  prefix + "32" + postfix,
+  prefix + "39" + postfix,
+  prefix + "46" + postfix,
+  prefix + "53" + postfix,
+  prefix + "60" + postfix,
+  prefix + "67" + postfix,
+  prefix + "74" + postfix,
+];
+
+const thursdayImages = [
+  prefix + "05" + postfix,
+  prefix + "12" + postfix,
+  prefix + "19" + postfix,
+  prefix + "26" + postfix,
+  prefix + "33" + postfix,
+  prefix + "40" + postfix,
+  prefix + "47" + postfix,
+  prefix + "54" + postfix,
+  prefix + "61" + postfix,
+  prefix + "68" + postfix,
+  prefix + "75" + postfix,
+];
+
+const fridayImages = [
+  prefix + "06" + postfix,
+  prefix + "13" + postfix,
+  prefix + "20" + postfix,
+  prefix + "27" + postfix,
+  prefix + "34" + postfix,
+  prefix + "41" + postfix,
+  prefix + "48" + postfix,
+  prefix + "55" + postfix,
+  prefix + "62" + postfix,
+  prefix + "69" + postfix,
+  prefix + "76" + postfix,
+];
+
+const saturdayImages = [
+  prefix + "07" + postfix,
+  prefix + "14" + postfix,
+  prefix + "21" + postfix,
+  prefix + "28" + postfix,
+  prefix + "35" + postfix,
+  prefix + "42" + postfix,
+  prefix + "49" + postfix,
+  prefix + "56" + postfix,
+  prefix + "63" + postfix,
+  prefix + "70" + postfix,
+  prefix + "77" + postfix,
+];
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const d = new Date();
+let day = weekday[d.getDay()];
+
+if (day === "Sunday") images = sundayImages;
+if (day === "Monday") images = mondayImages;
+if (day === "Tuesday") images = tuesdayImages;
+if (day === "Wednesday") images = wednesdayImages;
+if (day === "Thursday") images = thursdayImages;
+if (day === "Friday") images = fridayImages;
+if (day === "Saturday") images = saturdayImages;
+if (!day) images = sundayImages;
+images = shuffle(images);
+
 const delay = 4000;
 
 function Slideshow() {
@@ -48,6 +139,8 @@ function Slideshow() {
   }
 
   React.useEffect(() => {
+
+
     resetTimeout();
     timeoutRef.current = setTimeout(
       () =>
@@ -66,7 +159,7 @@ function Slideshow() {
     <div className="slideshow">
       <div className="slideshowSlider" style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}>
         {images.map((image, index) => (
-            <div className="slide" key={index} style={{ backgroundImage: `url(${image})`}} />
+          <div className="slide" key={index} style={{ backgroundImage: `url(${image})` }} />
         ))}
       </div>
     </div>
