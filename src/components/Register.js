94a27922093a69e08ce77a086e2e08cb08d141da
basoosh <basoosh1@gmail.com>
2024-03-25 23:54:35 -0400

    Updated Register page to say donuts are distributed on ArbOne

--- src/components/Register.js
+++ src/components/Register.js
@@ -11,7 +11,7 @@ class Register extends React.Component {
                     <img src={Title} alt="How to Register for Donuts" className="logo-image" />
                     <p className="left-body">Simply respond to any thread in /r/ethtrader with a message like the following:</p>
                     <p className="code">!register 0x12345678901234567890123456789012345678901</p>
-                    <p className="left-body">Replace the above address with your own and any earned DONUTs will be automatically sent to that address in the next distribution on Gnosis Chain.</p>
+                    <p className="left-body">Replace the above address with your own and any earned DONUTs will be automatically sent to that address in the next distribution on Arbitrum One.</p>
                     <p className="left-body">Remember that this process is publicly viewable by all and that other users will be able to see the Ethereum address tied to your Reddit account.  It is probably best to create a brand new Ethereum address just for DONUTs!</p>
             </div>
         );
