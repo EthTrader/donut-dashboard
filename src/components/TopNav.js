b41ebb3d79c0c62a2a0caaf3aa2a3cc40e95a193
basoosh <basoosh1@gmail.com>
2024-08-28 00:08:17 -0400

    Added membership to topnav

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -10,7 +10,8 @@ function TopNav() {
                 <div className="topnav">
                     {location.pathname === "/" ? <Link className="active" to="/">Home</Link> : <Link to="/">Home</Link>}
                     {location.pathname === "/faq" ? <Link className="active" to="faq">FAQ</Link> : <Link to="faq">FAQ</Link>}
-                    {location.pathname === "/register" ? <Link className="active" to="register">How to Register</Link> : <Link to="register">How to Register</Link>}
+                    {location.pathname === "/register" ? <Link className="active" to="register">Register</Link> : <Link to="register">Register</Link>}
+                    {location.pathname === "/membership" ? <Link className="active" to="membership">Membership</Link> : <Link to="membership">Membership</Link>}
                     {location.pathname === "/governance" ? <Link className="active" to="governance">Governance Scores</Link> : <Link to="governance">Governance Scores</Link>}
                     {location.pathname === "/distribution" ? <Link className="active" to="distribution">Distributions</Link> : <Link to="distribution">Distributions</Link>}
                     {location.pathname === "/delicious-donuts" ? <Link className="active" to="delicious-donuts">Delicious Donuts</Link> : <Link to="delicious-donuts">Delicious Donuts</Link>}
