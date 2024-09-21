a5f2c7577f7d18744891cbd7096bc2dd30bf59dc
basoosh <basoosh1@gmail.com>
2024-09-21 09:00:40 -0400

    Added images for topic limiter, css changes

--- src/components/TopicLimits.js
+++ src/components/TopicLimits.js
@@ -1,5 +1,16 @@
 import React from 'react';
-import Title from '../img/title-distributions.png';
+import Title from '../img/title-topiclimits.png';
+import tlAirdrop from '../img/tl-airdrop.png';
+import tlBridge from '../img/tl-bridge.png';
+import tlETF from '../img/tl-etf.png';
+import tlInfluencer from '../img/tl-influencer.png';
+import tlLayerTwo from '../img/tl-layertwo.png';
+import tlMemecoins from '../img/tl-memecoins.png';
+import tlPolitics from '../img/tl-politics.png';
+import tlExchanges from '../img/tl-exchanges.png';
+import tlRegulation from '../img/tl-regulation.png';
+import tlStake from '../img/tl-stake.png';
+import tlWallet from '../img/tl-wallet.png';
 import axios from 'axios';
 import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css'
@@ -58,7 +69,7 @@ class TopicLimits extends React.Component {
                         <AccordionItemPanel>
                             <p className="left-body-middle">The topic limiter is a feature designed to improve content organization on r/EthTrader, by automatically limiting the number of posts about specific topics.
 
-                                The limiter works by detecting and removing posts that exceed the allowed limit. This helps reduce duplicate content, spam, and creates a more diverse and engaging feed for all users..</p>
+                                The limiter works by detecting and removing posts that exceed the allowed limit. This helps reduce duplicate content, spam, and creates a more diverse and engaging feed for all users.</p>
                         </AccordionItemPanel>
                     </AccordionItem>
                 </Accordion>
@@ -67,11 +78,12 @@ class TopicLimits extends React.Component {
 
                 <p className="footer-text">Last Updated: {this.state.lastUpdate}</p>
 
-                <div className="membership-card">
+                <div className="membership-card topic-card">
 
                     <table className="topic-table">
                         <thead>
                             <tr>
+                                <th></th>
                                 <th className="donut-header topicText">
                                     Topic
                                 </th>
@@ -93,6 +105,19 @@ class TopicLimits extends React.Component {
                                 }
 
                                 return (<tr key={row.display_name} className={rowClass}>
+                                    <th>
+                                        { row.display_name === 'Airdrops' ? <img src={tlAirdrop} alt="Airdrops" className="topic-image" /> : <span />}
+                                        { row.display_name === 'Bridges' ? <img src={tlBridge} alt="Bridges" className="topic-image" /> : <span />}                                        
+                                        { row.display_name === 'ETF' ? <img src={tlETF} alt="ETF" className="topic-image" /> : <span />}
+                                        { row.display_name === 'Influencers' ? <img src={tlInfluencer} alt="Influencers" className="topic-image" /> : <span />}
+                                        { row.display_name === 'Layer 2\'s' ? <img src={tlLayerTwo} alt="LayerTwo" className="topic-image" /> : <span />}
+                                        { row.display_name === 'Meme Coins' ? <img src={tlMemecoins} alt="Memecoins" className="topic-image" /> : <span />}
+                                        { row.display_name === 'Politics' ? <img src={tlPolitics} alt="Politics" className="topic-image" /> : <span />}
+                                        { row.display_name === 'Exchanges' ? <img src={tlExchanges} alt="Exchanges" className="topic-image" /> : <span />}
+                                        { row.display_name === 'Regulation' ? <img src={tlRegulation} alt="Regulation" className="topic-image" /> : <span />}
+                                        { row.display_name === 'Staking/Restaking' ? <img src={tlStake} alt="Stake" className="topic-image" /> : <span />}
+                                        { row.display_name === 'Wallets' ? <img src={tlWallet} alt="Wallet" className="topic-image" /> : <span />}                                                                                                                                                                                                                                                                                                                                
+                                    </th>
                                     <th className="contentColumn pinkText topicText">
                                         {row.display_name}
                                     </th>
